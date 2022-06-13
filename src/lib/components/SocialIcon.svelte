c97d93a3c311af2e36ff2b7936a246b65bcdbbe9
Nathan Gaul <nathanjgaul@gmail.com>
2022-06-12 21:48:04 -0600

    Switch to iconify icons

--- src/lib/components/SocialIcon.svelte
+++ src/lib/components/SocialIcon.svelte
@@ -1,10 +1,10 @@
 <script>
-    import Fa from 'svelte-fa/src/fa.svelte';
+    import Icon from '@iconify/svelte';
 
     export let icon;
     export let url;
 </script>
 
 <a class="aspect-square text-slate-200 rounded-full border border-slate-200 p-4 mx-1 transition hover:scale-105" href={url} target="#blank">
-	<Fa icon={icon} size="2x" />
+	<Icon {icon} width=32 height=32 />
 </a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,4 @@
 <script>
-    import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-	import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 	import SocialIcon from '$lib/components/SocialIcon.svelte';
 </script>
 
@@ -10,10 +8,10 @@
 			<h1 class="title-font text-6xl mb-4 font-medium text-slate-200">Nathan Gaul</h1>
 			<h3 class="text-xl font-normal text-slate-300">Developer | Web Designer | Entrepreneur</h3>
 			<span class="inline-flex lg:ml-auto mt-6 w-full justify-center">
-				<SocialIcon icon={faGithub} url="https://github.com/NathanJGaul" />
-                <SocialIcon icon={faTwitter} url="https://twitter.com/NathanJGaul" />
-                <SocialIcon icon={faLinkedin} url="https://www.linkedin.com/in/nathanjgaul/" />
-                <SocialIcon icon={faEnvelope} url="mailto:nathanjgaul@gmail.com" />
+				<SocialIcon icon="fa6-brands:github" url="https://github.com/NathanJGaul" />
+				<SocialIcon icon="fa6-brands:twitter" url="https://twitter.com/NathanJGaul" />
+				<SocialIcon icon="fa6-brands:linkedin" url="https://www.linkedin.com/in/nathanjgaul/" />
+				<SocialIcon icon="fa6-solid:envelope" url="mailto:nathanjgaul@gmail.com" />
 			</span>
 		</div>
 	</div>
